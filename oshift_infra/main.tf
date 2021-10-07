dbb8b670ffc3d4722e1fb1a6bf042a537436e5e5
LB <lakshmi.balaramane@nutanix.com>
2021-10-07 19:36:38 +0900

    Thu  7 Oct 2021 19:36:38 JST

--- oshift_infra/main.tf
+++ oshift_infra/main.tf
@@ -22,65 +22,29 @@ provider "nutanix" {
   wait_timeout = 60
 }
 
-resource "nutanix_image" "rhocs" {
-  name        = "RHOCS"
-  description = "RHOCS.iso"
-  image_type = "ISO_IMAGE"
-  source_uri  = "https://s3.us-east-1.amazonaws.com/assisted-installer/discovery-image-b1975412-0c4a-4518-aec8-3ff6551ed375.iso?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA52ZYGBOVI2P2TOEQ%2F20210916%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210916T044509Z&X-Amz-Expires=14400&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3Bfilename%3Ddiscovery-image-b1975412-0c4a-4518-aec8-3ff6551ed375.iso&X-Amz-Signature=4197a7249d49e509daed07556d2bcc3ac00e31dc85b52483ee4d4999efa08551"
+## Creating BOOTSTRAP VMs here
+resource "nutanix_virtual_machine" "rhocs-bootstrap" {
+  count                = var.vm_bootstrap_count
+  name                 = "${var.vm_bootstrap_prefix}-${count.index + 1}"
+  cluster_uuid         = data.nutanix_cluster.cluster.id
+  num_vcpus_per_socket = "1"
+  num_sockets          = "6"
+  memory_size_mib      = 10*1024
+disk_list {
+  disk_size_bytes = 50 * 1024 * 1024 * 1024
+  device_properties {
+    device_type = "DISK"
+    disk_address = {
+      "adapter_type" = "SCSI"
+      "device_index" = "1"
+    }
+  }
 }
 
-# resource "nutanix_image" "centos" {
-#   name        = "centos"
-#   description = "centos.qcow"
-#   source_uri  = "https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/latest/latest/rhcos-live.x86_64.iso"
-# }
-
-# data "template_file" "cloud" {
-#     count = var.vm_count
-#     template = file("cloud-init")
-#     vars = {
-#         vm_name = "${var.vm_name}-${count.index + 1}"
-# 	      vm_domain = var.vm_domain
-#     }
-# }
-# resource "nutanix_virtual_machine" "rhocs" {
-#   count                = var.vm_count
-#   name                 = "${var.vm_name}-${count.index + 1}"
-#   cluster_uuid         = data.nutanix_cluster.cluster.id
-#   num_vcpus_per_socket = "1"
-#   num_sockets          = "4"
-#   memory_size_mib      = 16*1024
-
-#   disk_list {
-#     device_properties {
-#       device_type = "CDROM"
-#       disk_address = {
-#         "adapter_type" = "IDE"
-#         "device_index" = "0"
-#       }
-#     }
-#     data_source_reference = {
-#       kind = "image"
-
-#       uuid = nutanix_image.rhocs.id
-#     }
-#   }
-
-# disk_list {
-#   disk_size_bytes = 130 * 1024 * 1024 * 1024
-#   device_properties {
-#     device_type = "DISK"
-#     disk_address = {
-#       "adapter_type" = "SCSI"
-#       "device_index" = "1"
-#     }
-#   }
-# }
-
-#   nic_list {
-#     subnet_uuid = data.nutanix_subnet.subnet.id
-#   }
-# }
+  nic_list {
+    subnet_uuid = data.nutanix_subnet.subnet.id
+  }
+}
  
 ## Creating MASTER VMs here
 resource "nutanix_virtual_machine" "rhocs-master" {
@@ -89,25 +53,25 @@ resource "nutanix_virtual_machine" "rhocs-master" {
   cluster_uuid         = data.nutanix_cluster.cluster.id
   num_vcpus_per_socket = "1"
   num_sockets          = "4"
-  memory_size_mib      = 16*1024
+  memory_size_mib      = 8*1024
 
-  disk_list {
-    device_properties {
-      device_type = "CDROM"
-      disk_address = {
-        "adapter_type" = "IDE"
-        "device_index" = "0"
-      }
-    }
-    data_source_reference = {
-      kind = "image"
+  # disk_list {
+  #   device_properties {
+  #     device_type = "CDROM"
+  #     disk_address = {
+  #       "adapter_type" = "IDE"
+  #       "device_index" = "0"
+  #     }
+  #   }
+  #   data_source_reference = {
+  #     kind = "image"
 
-      uuid = nutanix_image.rhocs.id
-    }
-  }
+  #     uuid = nutanix_image.rhocs.id
+  #   }
+  # }
 
 disk_list {
-  disk_size_bytes = 130 * 1024 * 1024 * 1024
+  disk_size_bytes = 50 * 1024 * 1024 * 1024
   device_properties {
     device_type = "DISK"
     disk_address = {
@@ -129,25 +93,25 @@ resource "nutanix_virtual_machine" "rhocs-worker" {
   cluster_uuid         = data.nutanix_cluster.cluster.id
   num_vcpus_per_socket = "1"
   num_sockets          = "4"
-  memory_size_mib      = 16*1024
+  memory_size_mib      = 8*1024
 
-  disk_list {
-    device_properties {
-      device_type = "CDROM"
-      disk_address = {
-        "adapter_type" = "IDE"
-        "device_index" = "0"
-      }
-    }
-    data_source_reference = {
-      kind = "image"
+  # disk_list {
+  #   device_properties {
+  #     device_type = "CDROM"
+  #     disk_address = {
+  #       "adapter_type" = "IDE"
+  #       "device_index" = "0"
+  #     }
+  #   }
+  #   data_source_reference = {
+  #     kind = "image"
 
-      uuid = nutanix_image.rhocs.id
-    }
-  }
+  #     uuid = nutanix_image.rhocs.id
+  #   }
+  # }
 
 disk_list {
-  disk_size_bytes = 130 * 1024 * 1024 * 1024
+  disk_size_bytes = 50 * 1024 * 1024 * 1024
   device_properties {
     device_type = "DISK"
     disk_address = {
